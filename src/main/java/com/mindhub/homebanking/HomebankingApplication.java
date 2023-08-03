4b6e6bb7e87edd6688d45a9ff123cb4468e63406
Gerardomedinav <gerardomedinavv@hotmail.com>
2023-08-02 23:26:56 -0300

    crear la class Client

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,13 +1,25 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+
+	}
+	@Bean
+	public CommandLineRunner initData(ClientRepository clienteRepository){
+		return (args)-> {
+			Client client = new Client("Geraardo", "Medina", "gerardomedinav@hotmail.es", "12345");
+			clienteRepository.save(client);
+		};
 	}
 
 }
